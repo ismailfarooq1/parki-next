c5095eef62bce0b042f64f6183858c9ae141126a
Ismail Farooq <ismaeelfarooq53@gmail.com>
2022-11-15 11:09:09 +0500

    Responsice

--- pages/index.jsx
+++ pages/index.jsx
@@ -106,7 +106,7 @@ export default function Home() {
                     flexDirection: 'column'
                 }}
             >
-                <Box>
+                <Box sx={{ display: 'none' }}>
                     <Title order={1}> PARKI </Title>
                     <Title order={6}> Interiors </Title>
                 </Box>
